74e4ecf4fe7e0203443b2fd1727105691cdbb8af
Akira Watanabe <a0323w@gmail.com>
2022-12-24 14:09:35 +0900

    [update] Title,Resultシーンでクリックによるシーン遷移

--- Assets/Scripts/UI/ClickPanel.cs
+++ Assets/Scripts/UI/ClickPanel.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-/// <summary>
-/// オブジェクトをクリックした時に、指定した処理を実行する
-/// </summary>
 public class ClickPanel : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private UnityEvent _event;
 
+    /// <summary>
+    /// オブジェクトをクリックしたら、設定した処理を実行する
+    /// </summary>
     public void OnPointerClick(PointerEventData eventData)
     {
         _event?.Invoke();
