56664f424598b9e6034f2d063ead113a6735d551
Akira Watanabe <a0323w@gmail.com>
2022-12-24 23:23:10 +0900

    [update] FadeIn(簡易...今後修正有) → FadeOutがうまくいかない

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,53 @@ public class Fade : MonoBehaviour
     [Tooltip("フェードさせるUI")]
     [SerializeField] private Image _fadePanel;
     [Tooltip("実行時間")]
-    [Range(1f, 3f)]
-    [SerializeField] private float _fadeSpeed = 0f;
+    [SerializeField] private float _fadeTime = 1f;
+
+    private void Start()
+    {
+        _fadePanel.gameObject.SetActive(true);
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float alpha = 1f;
+        Color color = _fadePanel.color;
+
+        do
+        {
+            alpha -= Time.deltaTime * (1f /_fadeTime);
+            if (alpha <= 0f)
+            {
+                alpha = 0f;
+            }
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+        while (alpha > 0f);
+
+        _fadePanel.gameObject.SetActive(false);
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float alpha = 0f;
+        Color color = _fadePanel.color;
+
+        do
+        {
+            alpha += Time.deltaTime * (1f / _fadeTime);
+            if (alpha >= 1f)
+            {
+                alpha = 1f;
+            }
+            color.a = alpha;
+            _fadePanel.color = color;
+            yield return null;
+        }
+        while (alpha < 1f);
+
+        Debug.Log("Clear Fade");
+    }
 }
