0daf465cba4863875ef9c86d94260f93ced1c9b5
Akira Watanabe <a0323w@gmail.com>
2022-12-31 23:08:07 +0900

    [add] アイテムの基底クラス

---
+++ Assets/Scripts/Item/ItemBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテムの基底クラス
+/// </summary>
+public abstract class ItemBase : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
