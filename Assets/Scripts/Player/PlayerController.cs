6d59f7053b5b180dcb697fdaed82d81fd645ac2b
Akira Watanabe <a0323w@gmail.com>
2022-12-31 12:08:01 +0900

    [update] 接地判定

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     [Header("ステータス一覧")]
     [SerializeField] private float _moveSpeed = 1f;
     [SerializeField] private float _jumpPower = 1f;
+    [SerializeField] private bool _isGround = false;
 
     private Rigidbody _rb;
 
     private void Start()
     {
+        _isGround = true;
         _rb = GetComponent<Rigidbody>();
     }
 
@@ -21,12 +23,18 @@ public class PlayerController : MonoBehaviour
         float ver = Input.GetAxisRaw("Vertical");
 
         float y = _rb.velocity.y;
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _isGround)
         {
             y = _jumpPower;
+            _isGround = false;
         }
 
         _rb.velocity = 
             new Vector3(hol * _moveSpeed, 0, ver * _moveSpeed) + Vector3.up * y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isGround = true;
     }
 }
