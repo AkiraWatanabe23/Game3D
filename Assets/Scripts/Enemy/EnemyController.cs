4796f2aa49821573a8325083a4675e569b0be8c8
Akira Watanabe <a0323w@gmail.com>
2023-01-02 13:15:22 +0900

    [update] 敵の動きテスト中

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,23 +1,64 @@
 ﻿using Consts;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyController : EnemyBase
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    //[SerializeField] private Transform _target;
+    [SerializeField] private Transform[] _movePos = new Transform[2];
+
+    private NavMeshAgent _agent = default;
+    private EnemyState _state = EnemyState.DEFAULT;
+    private int _currentMoveIndex = 0;
+    private float _moveSpeed = 0f;
+
+    public Transform[] MovePos => _movePos;
+    public NavMeshAgent Agent => _agent;
+    public EnemyState State { get => _state; set => _state = value; }
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _state = EnemyState.MOVE;
+        _currentMoveIndex = 0;
+        _moveSpeed = _agent.speed;
+
+        if (_agent != null) Debug.Log("aruno");
+        else Debug.Log("naino");
+    }
 
     private void Update()
     {
-        //destination...行き先(NavMeshの進行方向の決定)
-        Agent.SetDestination(_target.position);
+        if (_agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            SwitchTarget();
+        }
+    }
+
+    //TODO：指定の箇所を行ったり来たりするようにする
+    private void SwitchTarget()
+    {
+        _agent.speed = _moveSpeed;
+        _currentMoveIndex++;
+        _agent.SetDestination
+            (_movePos[_currentMoveIndex % _movePos.Length].position);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //TODO：指定の箇所を行ったり来たりするようにする
-
         if (other.CompareTag(Define.PLAYER_TAG))
         {
             //TODO：Playerに追いついたときになにか処理をする
         }
+    }
+
+    public enum EnemyState
+    {
+        DEFAULT,
+        /// <summary> 通常の移動状態 </summary>
+        MOVE,
+        /// <summary> Player追跡中 </summary>
+        CHASE,
     }
 }
