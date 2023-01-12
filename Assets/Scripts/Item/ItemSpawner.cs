ddd9ea16e94b93e7dd5ba2a270aa2f48fd3f7bb7
Akira Watanabe <a0323w@gmail.com>
2023-01-12 14:12:25 +0900

    [add] 一定時間ごとにアイテムをスポーンする

---
+++ Assets/Scripts/Item/ItemSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary> ランダムなアイテムを一定時間ごとにスポーンする </summary>
+public class ItemSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _items = new GameObject[3];
+    [SerializeField] private List<Transform> _spawnPos = new();
+    [SerializeField] private float _spawnTime = 1f;
+
+    private void Start()
+    {
+        //ItemSpawn()を_spawnTime(s)後に呼び出し、その後_spawnTime(s)間隔で実行する
+        InvokeRepeating(nameof(ItemSpawn), _spawnTime, _spawnTime);
+    }
+
+    private void ItemSpawn()
+    {
+        //TODO：同じ場所にアイテムがスポーンされるのを防ぐ
+        foreach (var spawn in _spawnPos)
+        {
+            var go = Instantiate(_items[Random.Range(0, _items.Length)]);
+            go.transform.position = spawn.position;
+        }
+    }
+}
