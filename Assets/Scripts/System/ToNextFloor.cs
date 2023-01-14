f6b7b3ad4970679eb95bfb9872260919134ffe36
Akira Watanabe <a0323w@gmail.com>
2023-01-14 15:59:06 +0900

    [update] 落ちたらシーン遷移するようにする

---
+++ Assets/Scripts/System/ToNextFloor.cs
@@ -0,0 +1,13 @@
+﻿using Consts;
+using UnityEngine;
+
+public class ToNextFloor : MonoBehaviour
+{
+    [SerializeField] private SceneNames _scene = default;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneLoaders.PassToLoad
+                (Define.Scenes[_scene]);
+    }
+}
