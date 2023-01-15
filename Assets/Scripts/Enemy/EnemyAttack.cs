431ae8d23520be0a162d4e57e0f7f9ec4ec49fb1
Akira Watanabe <a0323w@gmail.com>
2023-01-15 12:44:29 +0900

    [update] 穴に落ちたらシーン遷移(Enemyの動き確認)

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyAttack
 {
     [SerializeField] private int _attackValue = 10;
+    [SerializeField] private float _attackInterval = 5f;
 
     public int AttackValue { get=> _attackValue; set => _attackValue = value; }
 
@@ -15,7 +16,11 @@ public class EnemyAttack
 
     public void Update()
     {
+        //一定時間経ったらEnemyの攻撃
+        //if (GameManager.Timer >= _attackInterval)
+        //{
 
+        //}
     }
 
     public void OnCollisionEnter(Collision col)
