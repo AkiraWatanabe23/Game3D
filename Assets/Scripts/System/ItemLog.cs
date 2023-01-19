e0e7ef9199fd9d3c8fdea91fad08d5feb8c89836
Akira Watanabe <a0323w@gmail.com>
2023-01-19 19:48:49 +0900

    [update] アイテムページの仕様修正

--- Assets/Scripts/System/ItemLog.cs
+++ Assets/Scripts/System/ItemLog.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 /// <summary> アイテムボックスな内でアイテムを選んだ時のログ表示 </summary>
-public class TextLog : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ItemLog : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Tooltip("アイテムのログを表示するUI")]
     [SerializeField] private Image _logImage = default;
     [Tooltip("アイテムボックス内でのログ")]
     [SerializeField] private Text _logText = default;
-    [SerializeField] private UnityEvent _openEvent = default;
-    [SerializeField] private UnityEvent _closeEvent = default;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //TODO：アイテムの説明をText表示
-        _openEvent?.Invoke();
+        //アイテムの説明を表示
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(true);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        //TODO：表示していたLogを閉じる
-        _closeEvent?.Invoke();
+        //表示していたLogを閉じる
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 }
