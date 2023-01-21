f4de517843732cedad49cadd780d8046cc0027c5
Akira Watanabe <a0323w@gmail.com>
2023-01-21 15:22:52 +0900

    [update] シーン内UIテスト中

--- Assets/Scripts/System/ItemLog.cs
+++ Assets/Scripts/System/ItemLog.cs
@@ -5,14 +5,12 @@ using UnityEngine.EventSystems;
 /// <summary> アイテムボックスな内でアイテムを選んだ時のログ表示 </summary>
 public class ItemLog : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [Tooltip("アイテムのログを表示するUI")]
-    [SerializeField] private Image _logImage = default;
     [Tooltip("アイテムボックス内でのログ")]
     [SerializeField] private Text _logText = default;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //アイテムの説明を表示
+        //アイテムの説明を表示(子オブジェクトに設定)
         for (int i = 0; i < gameObject.transform.childCount; i++)
         {
             gameObject.transform.GetChild(i).gameObject.SetActive(true);
